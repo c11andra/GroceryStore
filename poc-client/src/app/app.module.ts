c60cc5ad1e2f75e5f75b11e175ed86f1e9c1eb66
c11andra <c11andra@github.com>
2019-05-04 19:15:52 +0530

    Add  side navigation

--- poc-client/src/app/app.module.ts
+++ poc-client/src/app/app.module.ts
@@ -1,18 +1,21 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
+import { SideNavComponent } from './side-nav/side-nav.component';
+import { MatSidenavModule, MatIconModule } from '@angular/material';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SideNavComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatSidenavModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
