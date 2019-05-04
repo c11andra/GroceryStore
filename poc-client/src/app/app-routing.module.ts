4c952870a436c4204e7b2eb731d2fde9eba62004
c11andra <c11andra@github.com>
2019-05-04 22:28:30 +0530

    add manage items component

--- poc-client/src/app/app-routing.module.ts
+++ poc-client/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ManageItemsComponent } from './manage-items/manage-items.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+    { path: 'manage-items', component: ManageItemsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- poc-client/src/app/app.module.ts
+++ poc-client/src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SideNavComponent } from './side-nav/side-nav.component';
 import { MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { ManageItemsComponent } from './manage-items/manage-items.component';
 @NgModule({
   declarations: [
     AppComponent,
-    SideNavComponent
+    SideNavComponent,
+    ManageItemsComponent
   ],
   imports: [
     BrowserModule,
